5d9993c276b0e9e05d2bb9fe792a13563364507a
Julien <julien.pradet@gmail.com>
2017-03-18 14:14:08 +0100

    Initial commit

---
+++ demo/index.js
@@ -0,0 +1,1 @@
+import ReactFlip from '../src/index.js';
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval',
+  entry: {
+    'react-flip': path.resolve(__dirname, 'src/index.js'),
+    'demo/react-flip': path.resolve(__dirname, 'demo/index.js')
+  },
+  output: {
+    library: 'ReactFlip',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'eslint-loader',
+        options: {
+          emitError: true
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['react-app']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: path.resolve(__dirname, 'demo/index.html'),
+      filename: 'demo/index.html'
+    }),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true, // React doesn't support IE8
+        warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    })
+  ]
+};
